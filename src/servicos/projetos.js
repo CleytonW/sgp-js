a9dddd686cca80a6eb8b0828fde3efb606004bb2
Cleyton Melo <cleytonw11@gmail.com>
2025-08-09 19:34:28 -0300

    Adicionando funcionalidade para novos projetos e ajustando crud com o back

--- src/servicos/projetos.js
+++ src/servicos/projetos.js
@@ -2,8 +2,6 @@ import { api } from "./api";
 
 export async function listarProjetos(setProjetos) {
     await api.get('/projetos')
-        .then((response) => setProjetos(response.data.content))
+        .then((response) => setProjetos(response.data))
         .catch((error) => console.error(error));
 }
-
-
--- src/servicos/usuarios.js
+++ src/servicos/usuarios.js
@@ -1,38 +1,51 @@
 import { api } from "./api";
 
 export async function listarUsuarios(setUsuarios) {
-    await api.get('/usuarios')
-        .then((response) => setUsuarios(response.data.content))
-        .catch((error) => console.error(error));
+  await api
+    .get("/usuarios")
+    .then((response) => setUsuarios(response.data))
+    .catch((error) => console.error(error));
 }
 
-export async function buscarUsuarioPeloId(id, setNome, setCpf, setEmail, setSenha, setDataNascimento, setStatus) {
-    await api.get(`/usuarios/${id}`)
-        .then((response) => {
-            setNome(response.data.nome);
-            setCpf(response.data.cpf);
-            setEmail(response.data.email);
-            setSenha(response.data.senha);
-            setDataNascimento(response.data.dataNascimento);
-            setStatus(response.data.status);
-        })
-        .catch((error) => console.error(error));
+export async function buscarUsuarioPeloId(
+  id,
+  setNome,
+  setCpf,
+  setEmail,
+  setSenha,
+  setDataNascimento,
+  setStatus
+) {
+  await api
+    .get(`/usuarios/${id}`)
+    .then((response) => {
+      setNome(response.data.nome);
+      setCpf(response.data.cpf);
+      setEmail(response.data.email);
+      setSenha(response.data.senha);
+      setDataNascimento(response.data.dataNascimento);
+      setStatus(response.data.status);
+    })
+    .catch((error) => console.error(error));
 }
 
 export async function cadastrarUsuario(usuario, navigate) {
-    await api.post('/usuarios', usuario)
-        .then(() => navigate('/usuarios'))
-        .catch((error) => console.error(error));
+  await api
+    .post("/usuarios", usuario)
+    .then(() => navigate("/usuarios"))
+    .catch((error) => console.error(error));
 }
 
 export async function atualizarUsuario(id, usuario, navigate) {
-    await api.put(`/usuarios/${id}`, usuario)
-        .then(() => navigate("/usuarios"))
-        .catch((error) => console.error(error));
+  await api
+    .put(`/usuarios/${id}`, usuario)
+    .then(() => navigate("/usuarios"))
+    .catch((error) => console.error(error));
 }
 
 export async function deletarUsuario(id) {
-    await api.delete(`/usuarios/${id}`)
-        .then(() => { })
-        .catch((error) => console.error(error));
+  await api
+    .delete(`/usuarios/${id}`)
+    .then(() => {})
+    .catch((error) => console.error(error));
 }
